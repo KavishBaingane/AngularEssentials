1c5479645917d1f787b50a98a9766bfe3f8eab2d
KavishBaingane <104kbaingane@gmail.com>
2024-05-28 19:37:23 +0530

    W1:Output emitter function in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,5 +16,9 @@ import { DUMMY_USERS } from './user/dummy-users';
 export class AppComponent {
   users = DUMMY_USERS;
   title = 'AngularEssentials';
+
+  onSelectUser(id:string){
+    console.log("Selected user with Id"+ id );
+  }
 }
 
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input , Output } from '@angular/core';
 import { RequiredValidator } from '@angular/forms';
 
 @Component({
@@ -10,14 +10,17 @@ import { RequiredValidator } from '@angular/forms';
 })
 export class UserComponent {
 
-
+@Input({required:true}) id!: string;
     @Input({required:true}) avatar!: string;
     @Input({required:true}) name!: string;
+    @Output() select = new EventEmitter();  //it has been using for a long time --> so use this instead
+
+    // select = output<string>();  //this is not used though it is new 
 
     get imagePath(){
            return 'assets/users/' + this.avatar;
      }
      onSelectUser(){
- 
+        this.select.emit(this.id);
      }
 }
