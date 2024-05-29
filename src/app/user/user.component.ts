47afc75b18edce8172260f06baa644f856a4ffc2
KavishBaingane <104kbaingane@gmail.com>
2024-05-29 14:30:58 +0530

    W1:Objects as input

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -10,17 +10,19 @@ import { RequiredValidator } from '@angular/forms';
 })
 export class UserComponent {
 
-@Input({required:true}) id!: string;
-    @Input({required:true}) avatar!: string;
-    @Input({required:true}) name!: string;
+@Input({required:true}) user!: {
+  id:string;
+  avatar: string;
+  name: string;
+};
     @Output() select = new EventEmitter();  //it has been using for a long time --> so use this instead
 
     // select = output<string>();  //this is not used though it is new 
 
     get imagePath(){
-           return 'assets/users/' + this.avatar;
+           return 'assets/users/' + this.user.avatar;
      }
      onSelectUser(){
-        this.select.emit(this.id);
+        this.select.emit(this.user.id);
      }
 }
