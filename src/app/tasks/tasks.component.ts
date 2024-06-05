704e4906046416c1f15a6d8f353f0126ff1587b5
KavishBaingane <104kbaingane@gmail.com>
2024-06-05 14:02:03 +0530

    W1:Dependency injection

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -3,6 +3,9 @@ import { TaskComponent } from './task/task.component';
 import { Title } from '@angular/platform-browser';
 import { NewTaskComponent } from './new-task/new-task.component';
 import { type NewTaskData } from './task/task.model';
+import { TasksService } from './tasks.service';
+
+
 @Component({
   selector: 'app-tasks',
   standalone: true,
@@ -15,55 +18,26 @@ export class TasksComponent {
   @Input({required:true}) name!: string;
   isAddingTask  = false;
 
-  tasks = [
-    {
-      id:'t1',
-      userId : 'u1',
-      title: 'Master Angular',
-      summary : 'Learn all the basic and advanced features of Angular & how to apply them.',
-      dueDate: '2025-12-31'
-    },
-    {
-      id:'t2',
-      userId:'u2',
-      title: 'Build the protype',
-      summary : 'Nuild a first protype of the onle shop website',
-      dueDate: '2024-05-31'
-    },
-    {
-      id:'t3',
-      userId : 'u3',
-      title: 'Prepare issue templte',
-      summary : 'Prepare the first template regarding issues consisting in the project',
-      dueDate: '2024-12-31'
-    },
-  ];
 
+  constructor(private tasksService: TasksService){
+
+  }
   get selectedUserTasks(){
-    return this.tasks.filter((task)=>task.userId===this.userId)!;
+    return this.tasksService.getUserTasks(this.userId);
   }
   
   onCompleteTask(id:string){
-    this.tasks = this.tasks.filter((task)=>task.id!==id);
+    
   }
   onStartAddTask(){
     this.isAddingTask = true;
   }
 
-  onCancelAddTask(){
+  onCloseAddTask(){
     this.isAddingTask = false;
   }
 
-  onAddTask(taskData:NewTaskData){
-    this.tasks.unshift({
-      id: new Date().getTime().toString(),
-      userId :this.userId,
-      title: taskData.title,
-      summary:taskData.summary,
-      dueDate:taskData.date
-    });
-    this.isAddingTask = false;
-  }
+  
 }
 
 
